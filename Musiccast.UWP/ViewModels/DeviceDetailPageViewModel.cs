13a0e8d70e45e3e5d7ab8c858053b5ab2ae6e421
Wim De Smet <youtoofan@hotmail.com>
2018-07-28 13:21:42 +0200

    more details, with sliding panel

--- Musiccast.UWP/ViewModels/DeviceDetailPageViewModel.cs
+++ Musiccast.UWP/ViewModels/DeviceDetailPageViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using App4.Services;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Threading;
 using Musiccast.Models;
+using Musiccast.Service;
 
 namespace App4.ViewModels
 {
     public class DeviceDetailPageViewModel : ViewModelBase
     {
         private readonly NavigationServiceEx navigationService;
+        private MusicCastService service;
         private Device _device;
 
         public DeviceDetailPageViewModel(NavigationServiceEx navigationService)
@@ -32,6 +38,46 @@ namespace App4.ViewModels
             {
                 Set(ref _device, value);
             }
+        }
+
+        public ICommand NavigateHomeCommand
+        {
+            get
+            {
+                return new RelayCommand(() => navigationService.Navigate(typeof(ZonesViewModel).FullName));
+            }
+        }
+
+        public ICommand TogglePowerCommand
+        {
+            get
+            {
+                return new RelayCommand(async () =>
+                {
+                    await TogglePowerAsync();
+                    await RefreshDeviceAsync();
+                });
+            }
+        }
+
+        private async Task RefreshDeviceAsync()
+        {
+            var updatedDevice = await service.RefreshDeviceAsync(Device.Id, Device.Zone);
+
+            await DispatcherHelper.RunAsync(() =>
+            {
+                Device.Power = updatedDevice.Power;
+                Device.Input = updatedDevice.Input.ToString();
+                Device.SubTitle = updatedDevice.NowPlayingInformation;
+            });
+        }
+
+        private async Task TogglePowerAsync()
+        {
+            if (service == null)
+                service = new MusicCastService();
+
+            await service.TogglePowerAsync(Device.Id, Device.Zone);
         }
     }
 }
